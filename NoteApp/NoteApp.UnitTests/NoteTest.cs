ed07dc27deb089f409a6a1d64b74c4bf0f4b2da8
Dani <mptts28@gmail.com>
2024-04-07 10:04:37 +0700

    add ProjectTest

--- NoteApp/NoteApp.UnitTests/NoteTest.cs
+++ NoteApp/NoteApp.UnitTests/NoteTest.cs
@@ -4,11 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit;
+using NUnit.Framework;
+using NoteApp;
 
 namespace NoteApp.UnitTests
 {
     public class NoteTest
     {
+        public void TestSetName_Incorrect_50()
+        {
+            Note note = new Note("", NoteCategory.Other, "");
+            bool wasItThrow = false;
+            try
+            {
+                note.Name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            }
+            catch (ArgumentException)
+            {
+                wasItThrow = true;
+            }
+            Assert.Equals(wasItThrow, true);
+        }
+
+        public void TestSetName_Null()
+        {
+            Note note = new Note("", NoteCategory.Other, "");
+            Assert.Equals(note.Name, "Без названия");
+        }
+
+        public void TestSetName()
+        {
+            Note note = new Note("Some name", NoteCategory.Other, "");
+            Assert.Equals(note.Name, "Some name");
+        }
+
+        public void TestCategory()
+        {
+            Note note = new Note("Some name", NoteCategory.Other, "");
+            Assert.Equals(note.Category, NoteCategory.Other);
+        }
+
+        public void TestText()
+        {
+            Note note = new Note("Some name", NoteCategory.Other, "Some text");
+            Assert.Equals(note.Text, "Some text");
+        }
+
+        public void TestTimeOfCreation()
+        {
+            Note note = new Note("Some name", NoteCategory.Other, "");
+            Assert.Equals(note.TimeOfCreation, DateTime.Now);
+        }
+
+        public void TestTimeOfModification()
+        {
+            Note note = new Note("Some name", NoteCategory.Other, "");
+            Assert.Equals(note.TimeOfModification, DateTime.Now);
+        }
+
 
     }
+
+
 }
--- NoteApp/NoteApp.UnitTests/ProjectTest.cs
+++ NoteApp/NoteApp.UnitTests/ProjectTest.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
 
 namespace NoteApp.UnitTests
 {
     internal class ProjectTest
     {
+        public void TestNotes()
+        {
+            Note note = new Note("name", NoteCategory.Other, "");
+            List<Note> list = new List<Note> { note };
+            Project project = new Project(list);
+
+            var stillThatList = project.GetNotes();
+            Assert.Equals(list, stillThatList);
+        }
+
+        public void TestAddNote()
+        {
+            Note note = new Note("name", NoteCategory.Other, "");
+            List<Note> list = new List<Note> { note };
+            Project project = new Project(list);
+
+            Note otherNote = new Note("other", NoteCategory.Other, "");
+            project.AddNote(otherNote);
+            Assert.Equals(project.GetNotes()[1], otherNote);
+        }
+
+        public void TestSortNotes1()
+        {
+
+        }
     }
 }
